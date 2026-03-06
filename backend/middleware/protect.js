d567307b79a4cd35f3e18687cc4bfadb67723057
Abdullah22-22 <abdullahaltameemi28@gmail.com>
2026-03-06 16:25:28 +0200

    feat:added authentication

---
+++ backend/middleware/protect.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+
+const protect = (req, res, next) => {
+  try {
+    let token;
+
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = { id: decoded.sub };
+
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+};
+
+module.exports = protect;
